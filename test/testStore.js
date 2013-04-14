fb67369281b1b277985a909100ad09f3881d52d4
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2013-04-14 22:29:42 +0200

    Changed order of expected and actual in tests.

--- test/testStore.js
+++ test/testStore.js
@@ -2,19 +2,19 @@ var assert = require('assert')
 var store = require('../src/store').create();
 
 exports['test current on empty store is 0'] = function() {
-  assert.equal(0, store.current('nonexisting'), 'Incorrect default value')
+  assert.equal(store.current('nonexisting'), 0, 'Incorrect default value')
 }
 
 exports['test setting 1 current is 1'] = function() {
   store.store('one', 1)
-  assert.equal(1, store.current('one'), 'Incorrect stored value')
+  assert.equal(store.current('one'), 1, 'Incorrect stored value')
 }
 
 exports['test setting 1 value has that value as history'] = function() {
   store.store('history', 1)
   var history = store.history('history')
-  assert.equal(1, history.length, 'Incorrect number of stored values')
-  assert.equal(1, history[0], 'Incorrect stored value')
+  assert.equal(history.length, 1, 'Incorrect number of stored values')
+  assert.equal(history[0], 1, 'Incorrect stored value')
 }
 
 exports['test setting many values limits history to last 30 values'] = function() {
